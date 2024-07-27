4087266bd56b1e43c00611868ccb4c29cefb7d92
belal_soliman <belalsoliman8@gmail.com>
2024-07-27 15:26:25 +0300

    create edit notes page and navigator

---
+++ lib/views/editeNoteView.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:notes/views/widgets/editNoteBody.dart';
+
+class Editenoteview extends StatelessWidget {
+  const Editenoteview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: EditenoteviewBody(),
+    );
+  }
+}
--- lib/views/widgets/custom_notes_view.dart
+++ lib/views/widgets/custom_notes_view.dart
@@ -1,51 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/editeNoteView.dart';
 
 class NotesCards extends StatelessWidget {
   const NotesCards({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.yellowAccent,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          ListTile(
-              title: const Text(
-                "belal",
-                style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
-              ),
-              subtitle: const Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: Text(
-                  " notes paragraph and some words  to view static",
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const Editenoteview();
+        }));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.yellowAccent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ListTile(
+                title: const Text(
+                  "belal",
                   style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.black54,
+                      fontSize: 32,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                subtitle: const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    " notes paragraph and some words  to view static",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black54,
+                    ),
                   ),
                 ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
+                  color: Colors.black,
+                  iconSize: 30,
+                )),
+            const Padding(
+              padding: EdgeInsets.only(right: 16),
+              child: Text(
+                "may21,2022",
+                style: TextStyle(color: Colors.black54, fontSize: 24),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.delete),
-                color: Colors.black,
-                iconSize: 30,
-              )),
-          const Padding(
-            padding: EdgeInsets.only(right: 16),
-            child: Text(
-              "may21,2022",
-              style: TextStyle(color: Colors.black54, fontSize: 24),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
